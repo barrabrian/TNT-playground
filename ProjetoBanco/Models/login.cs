b4d74053bf115b652c2aad3826f6bceb481b6571
barrabrian <barra_brian@hotmail.com>
2017-07-11 16:37:46 -0300

    Initial Commit
    First Commit

---
+++ ProjetoBanco/Models/login.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjetoBanco.Models
+{
+    [Table("login")]
+    public class login
+    {
+        [Index(IsUnique = true)]
+        public int id { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório.")]
+        [Index(IsUnique = true)]
+        public string username { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório.")]
+        public string password { get; set; } 
+
+    }
+}
